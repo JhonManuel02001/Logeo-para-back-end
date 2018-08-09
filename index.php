6cded96afea10d9cc8d96ecb3b3e51725aae611f
Aprendiz <Aprendiz@BOGDFPCGM8F0040.FormacionSena.RED>
2018-08-09 13:21:24 -0500

    para logearse

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <title>Login</title>
+</head>
+<body>
+
+	<form>
+		<div class="tit"><h1>Ingresar</h1></div><br>
+
+		<div><label>Email:</label><input type="text" name="Email" value=""></div>
+		<div><label>Contraseña:</label><input type="password" name="password" value=""></div>
+		<div><label>Confirmar contraseña:</label><input type="password" name="con_pass" value=""><div>
+
+		<div class="reg"><input type="submit" name="registrarse"></div>
+		<div class="ing"><input type="reset" name="iniciar"></div>
+	</form>	
+</body>	 
+</html>
